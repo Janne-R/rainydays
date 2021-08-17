f37f7c8674d02881532819b8d071dcd95f201c26
Janne-R <74651035+Janne-R@users.noreply.github.com>
2021-08-17 14:10:30 +0200

    max per page fix

--- js/men.js
+++ js/men.js
@@ -1,4 +1,4 @@
-import { productArray } from "./constants/productArray.js";
+/*import { productArray } from "./constants/productArray.js";
 const productListMen = document.querySelector(".product-list-men");
 
 productArray.forEach(function (product) {
@@ -14,6 +14,42 @@ productArray.forEach(function (product) {
 `
   }
 })
+*/
+
+const productListMen = document.querySelector(".product-list-men");
+const url = "https://rainydays.janne-ringdal.one/wp-json/wc/store/products/?per_page=20";
+
+async function callApi() {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+
+    console.log(json);
+    return json;
+
+  } catch (error) {
+    productListMen.innerHTML = "error";
+
+  }
+}
+
+const products = await callApi();
+
+products.forEach(function (result) {
+  if (result.categories[0].id === 16) {
+    productListMen.innerHTML +=
+
+      `<a class="products-men" href="product-man.html?id=${result.id}">
+      <img src="${result.images[0].src}" alt="rainjacket" />
+      <h2>${result.name}</h2>
+      <p>${result.short_description}</p>
+      <p>$${result.prices.price}</p>
+  </a>`;
+
+  }
+});
+
+
 
 
 
